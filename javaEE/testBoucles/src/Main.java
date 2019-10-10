4eaba96046cef484169eae05152794b602b960ef
ccasas <c.casas@coperbee.fr>
2019-10-10 11:18:03 +0200

    fizzbuzz

--- javaEE/testBoucles/src/Main.java
+++ javaEE/testBoucles/src/Main.java
@@ -1,29 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
 
-        double[] ts = {6.3, -6.2, 8.5, 9.4, 100.25, -300.25};
-        ArrayList<Double> dNeg = new ArrayList<>();
-        ArrayList<Double> dPos = new ArrayList<>();
-        Arrays.sort(ts);
+//        si le nombre est divisible par 3 : on écrit Fizz
+//        si le nombre est divisible par 5 : on écrit Buzz
+//        si le nombre est divisible par 3 et par 5 : on écrit Fizzbuzz
+//        sinon : on écrit le nombre
 
-        for (double tss : ts) {
-            if (tss < 0) {
-                dNeg.add(tss);
-            } else {
-                dPos.add(tss);
-            }
-        }
+        fizBuz(15);
 
-        if (Math.abs(dNeg.get(dNeg.size() - 1)) == dPos.get(0)) {
-            System.out.println(dPos.get(0));
-        } else if (Math.abs(dNeg.get(dNeg.size() - 1)) < dPos.get(0)) {
-            System.out.println(dNeg.get(dNeg.size() - 1));
-        } else {
-            System.out.println(dPos.get(0));
+    }
+
+    public static void fizBuz(int number) {
+
+        if(number % 3 == 0 && number % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if(number % 5 == 0) {
+            System.out.println("Buzz");
+        } else if (number % 3 == 0 ) {
+            System.out.println("Fizz");
         }
     }
 }
+
